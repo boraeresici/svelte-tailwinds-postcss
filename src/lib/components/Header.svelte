ab632bec24e56116b3277481eb6e3f9077144ffe
Bora Eresici <borae@Boras-MacBook-Pro.local>
2024-07-21 19:50:30 +0300

    first repo upload - commit message

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,32 @@
+<!-- Header.svelte -->
+<header class="bg-blue-400light p-1 display: flex flex-wrap justify-end">
+	<a href="/">Home</a>
+
+	<nav>
+		<ul>
+			<li>
+				<a href="/blog">Blog</a>
+			</li>
+			<li>
+				<a href="/about">About</a>
+			</li>
+			<li>
+				<a href="/contact">Contact</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+<!-- ... The rest of the file's contents here -->
+<style>
+	ul {
+		margin: 0;
+		list-style-type: none;
+		display: flex;
+		gap: 1rem;
+	}
+
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+</style>
